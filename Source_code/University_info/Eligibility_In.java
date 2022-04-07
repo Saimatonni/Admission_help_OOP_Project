f4a0d22b5edf054810e9f2f6356d08a8311049ee
saima <74361691+Saimatonni@users.noreply.github.com>
2022-04-07 13:27:37 +0600

    Add files via upload

---
+++ Source_code/University_info/Eligibility_In.java
@@ -0,0 +1,4 @@
+package com.example.admission_help.universityInfo;
+
+public interface Eligibility_In {
+}
---
+++ Source_code/University_info/Eligibility_check.java
@@ -0,0 +1,8 @@
+package com.example.admission_help.universityInfo;
+
+public class Eligibility_check {
+   String in;
+   public void result(){
+
+   }
+}
