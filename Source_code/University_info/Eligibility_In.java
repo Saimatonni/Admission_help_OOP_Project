58ea708eede339182b7607ac59c59eeb294b41fb
Dipto Shaha <64748257+Dipto-shaha@users.noreply.github.com>
2022-04-09 00:27:31 +0600

    Update Eligibility_In.java

--- Source_code/University_info/Eligibility_In.java
+++ Source_code/University_info/Eligibility_In.java
@@ -2,4 +2,6 @@ package com.example.admission_help.universityInfo;
 
 public interface Eligibility_In {
     public String Inputcut(String s);
+    public String InputCheck(student obj);
+    public String Eligible_List(student obj);
 }
